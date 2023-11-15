acf14610179fc9d2099285129e010f23e8d52592
Nemuriciu2 <94646639+Nemuriciu2@users.noreply.github.com>
2023-11-15 20:40:21 +0100

    Refractor : add code for normal and uppercase name
    Added the code to support the new test : "multiple mixed names with normal and uppercase" + deleted the old code because the new one support the previous version too

--- greet.js
+++ greet.js
@@ -2,17 +2,48 @@ function greet(name) {
   if (name === null || name === undefined || name === '') {
     return 'Hello, my friend.';
   }
+
   if (Array.isArray(name)) {
-     let text = 'Hello';
-     for (let counter = 0; counter < name.length; counter++) {
-       if (counter == name.length-1) {
-        text += ` and ${name[counter]}.`
+    let tableLowerName = [];
+    let tableUpperName = [];
+
+    for (let counter = 0; counter < name.length; counter++) {
+     if(name[counter] === name[counter].toUpperCase()) {
+       tableUpperName.push(name[counter]);
+     }else{
+       tableLowerName.push(name[counter]);
+     }
+   }
+
+   let textMin = 'Hello';
+   let textMax = '';
+   if(tableUpperName.length != 0){
+     textMax = ' AND HELLO';
+
+     for (let counter = 0; counter < tableUpperName.length; counter++) {
+       if(counter == tableUpperName.length-1){
+         textMax += ` ${tableUpperName[counter]}!`;
        }else{
-         text += `, ${name[counter]}`;
-      }
-    }
-    return text;
-  }
+         textMax += ` ${tableUpperName[counter]}`;
+       }
+     }
+   }
+
+   for (let counter = 0; counter < tableLowerName.length; counter++) {
+     if(counter == tableLowerName.length-1){
+       textMin += ` and ${tableLowerName[counter]}.`;
+     }else{
+       textMin += `, ${tableLowerName[counter]}`;
+     }
+   }
+
+   if(textMax != '') {
+     return textMin += textMax;
+   }
+
+   return textMin;
+ }
+
   if (name === name.toUpperCase()) {
     return `HELLO, ${name}!`;
   }
