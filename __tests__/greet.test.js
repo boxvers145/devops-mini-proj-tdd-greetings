76bc7573dad21bfec8e5cf1f08d5b97adada52d4
Nemuriciu2 <94646639+Nemuriciu2@users.noreply.github.com>
2023-11-15 18:47:44 +0100

    update : greet.test.js new test
    added comment above each test + created the test number 5

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,9 +1,13 @@
 
 const greet = require('../greet');
 
+//Test number 1
+
 test('greet function returns greeting', () => {
     expect(greet('Jean-Kevin')).toBe('Hello, Jean-Kevin.');
 });
+
+//Test number 2
 
 test('greet function check null, undefined, and empty string', () => {
     expect(greet(null)).toBe('Hello, my friend.');
@@ -11,10 +15,20 @@ test('greet function check null, undefined, and empty string', () => {
     expect(greet('')).toBe('Hello, my friend.');
 });
 
+//Test number 3
+
 test('greet function check only uppercase name', () => {
     expect(greet('KENOBI')).toBe('HELLO, KENOBI!');
 });
 
+//Test number 4
+
 test('greet function check two names', () => {
     expect(greet(['Kratos', 'Thanathos'])).toBe('Hello, Kratos and Thanathos.');
+});
+
+//Test number 5
+
+test('greet function check more than two names', () => {
+    expect(greet(['Kratos', 'Thanathos', 'Hypnos'])).toBe('Hello, Kratos, Thanathos and Hypnos.');
 });
